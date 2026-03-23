7b579649ea9c8792aa8449561f7b4cc3f64fc4c1
2023Azure <3229873486@qq.com>
2026-03-23 16:29:10 +0800

    first commit

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/azure/code/minnow/tests
+# Build directory: /home/azure/code/minnow/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/byte_stream.cc
@@ -0,0 +1,63 @@
+#include "byte_stream.hh"
+
+using namespace std;
+
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), buffer_() {}
+
+bool Writer::is_closed() const
+{
+  return this->close_;
+}
+
+void Writer::push( string data )
+{
+  uint64_t can_push = min( available_capacity(), data.length() );
+  this->buffer_.append( data.substr( 0, can_push ) );
+  this->bytes_push_ += can_push;
+  (void)data;
+}
+
+void Writer::close()
+{
+  this->close_ = true;
+}
+
+uint64_t Writer::available_capacity() const
+{
+  return this->capacity_ - this->buffer_.length();
+}
+
+uint64_t Writer::bytes_pushed() const
+{
+  return this->bytes_push_;
+}
+
+bool Reader::is_finished() const
+{
+  return this->buffer_.empty() && this->close_;
+}
+
+uint64_t Reader::bytes_popped() const
+{
+  return this->bytes_pop_;
+}
+
+string_view Reader::peek() const
+{
+  if ( buffer_.empty() )
+    return {};
+  return std::string_view( buffer_ );
+}
+
+void Reader::pop( uint64_t len )
+{
+  uint64_t can_pop = min( len, buffer_.length() );
+  this->buffer_.erase( 0, can_pop );
+  this->bytes_pop_ += can_pop;
+  (void)len;
+}
+
+uint64_t Reader::bytes_buffered() const
+{
+  return (uint64_t)this->buffer_.length();
+}
